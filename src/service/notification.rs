69df08a2ffb8450aa2211ff7c948645cdc74bf01
sashishuu <emailkamu@gmail.com>
2025-03-28 20:35:05 +0700

    Implement notify function in Notification service to notify each Subscriber

--- src/service/notification.rs
+++ src/service/notification.rs
@@ -32,4 +32,23 @@ impl NotificationService {
         return Ok(result.unwrap());
     }
 
+    pub fn notify(&self, product_type: &str, status: &str, product: Product) {
+        let payload: Notification = Notification {
+            product_title: product.clone().title,
+            product_type: String::from(product_type),
+            product_url: product.clone().get_url(),
+            subscriber_name: String::from(""),
+            status: String::from(status),
+        };
+    
+        let subscribers: Vec<Subscriber> = SubscriberRepository::list_all(product_type);
+        for subscriber in subscribers {
+            let mut payload_clone = payload.clone();
+            payload_clone.subscriber_name = subscriber.clone().name;
+            let subscriber_clone = subscriber.clone();
+            thread::spawn(move || subscriber_clone.update(payload_clone));
+        }
+    }
+    
+
 }
