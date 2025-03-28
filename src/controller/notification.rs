72ca8f4cda9da981f0c80bbb641dbd31f98129b2
sashishuu <emailkamu@gmail.com>
2025-03-28 20:05:02 +0700

    Create Notification service struct skeleton

---
+++ src/controller/notification.rs
@@ -0,0 +1,5 @@
+use rocket::response::status::Created;
+use rocket::serde::json::Json;
+use bambangshop::Result;
+use crate::model::subscriber::Subscriber;
+use crate::service::notification::NotificationService;
